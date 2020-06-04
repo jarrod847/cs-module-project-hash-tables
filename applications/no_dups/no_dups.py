6fd0e2ceff2bb56181a6f8c4ba35053e4c528359
jarrodskahill <jarrodskahill@gmail.com>
2020-06-04 10:39:26 -0700

    day 3, 3 done

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,16 @@
 def no_dups(s):
     # Your code here
-
+    num = 0
+    dic = {}
+    x = s.split()
+    for word in x:
+        if word in dic:
+            continue
+        else:
+            num += 1
+            dic[word] = num
+    return ' '.join(dic.keys())
+    # print(dic)
 
 
 if __name__ == "__main__":
